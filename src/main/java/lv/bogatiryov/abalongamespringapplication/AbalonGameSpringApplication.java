cc663d1496a23eb3f7215b30d7e90431ab0761dd
arbog <ar.bogatirjovs@gmail.com>
2023-03-08 20:24:24 +0200

    init commit

---
+++ src/main/java/lv/bogatiryov/abalongamespringapplication/AbalonGameSpringApplication.java
@@ -0,0 +1,13 @@
+package lv.bogatiryov.abalongamespringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbalonGameSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbalonGameSpringApplication.class, args);
+    }
+
+}
